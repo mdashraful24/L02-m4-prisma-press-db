d3bb3efe84be7878698ce241ba30da7abfb84f91
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-06-21 22:58:17 +0600

    added setting up multi prisma schema file and defining user & profile model

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import config from "./config";
 
 const app: Application = express();
 
+// Middleware
 app.use(cors({
     origin: config.appUrl,
     credentials: true,
@@ -13,8 +14,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
 });
+
 
 export default app;
