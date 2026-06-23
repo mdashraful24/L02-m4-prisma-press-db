8c93750e3b695b808f688e86aa3867293db34e4b
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-06-24 02:01:58 +0600

    added setting access and refreh token in cookies

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -6,6 +6,22 @@ import { sendResponse } from "../../utils/sendResponse";
 const loginUser = catchAsync(async (req, res) => {
     const result = await authService.loginUserIntoDB(req.body);
 
+    const { accessToken, refreshToken } = result;
+
+    res.cookie("access-token", accessToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "none",
+        maxAge: 1000 * 60 * 60 * 24      // * 24 hours or 1 day
+    });
+
+    res.cookie("refresh-token", refreshToken,{
+        httpOnly: true,
+        secure: false,
+        sameSite: "none",
+        maxAge: 1000 * 60 * 60 * 24 * 7        // * 7 days
+    });
+
     sendResponse(res, {
         statusCode: httpStatus.OK,
         success: true,
