f86e947cb225e000005aab24fcb54f3ad25563f5
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-06-26 01:08:57 +0600

    updated access control

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -7,8 +7,8 @@ const router = Router();
 
 
 router.post("/register", userController.registerUser);
-router.get("/me", authProtected(Role.ADMIN, Role.USER, Role.AUTHOR), userController.getMyProfile);
-router.put("/my-profile", authProtected(Role.ADMIN, Role.AUTHOR, Role.USER), userController.updateMyProfile);
+router.get("/me", authProtected(Role.ADMIN, Role.USER), userController.getMyProfile);
+router.put("/my-profile", authProtected(Role.ADMIN, Role.USER), userController.updateMyProfile);
 
 
 export const userRouter = router;
